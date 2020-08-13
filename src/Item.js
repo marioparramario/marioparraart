eb14cc078ea1df3d24dd3f770c7cc382862054a0
oqgraphic <hello@oqgraphic.com>
2020-08-13 17:10:00 +0200

    at least works

--- src/Item.js
+++ src/Item.js
@@ -12,7 +12,7 @@ export function Item({ id }) {
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        exit={{ opacity: 0, transition: { duration: 1 } }}
+        exit={{ opacity: 0, transition: { duration: 1, delay: 2 } }}
         transition={{ duration: 1, delay: 0.15 }}
         style={{ pointerEvents: "auto" }}
         className="overlay"
@@ -21,10 +21,10 @@ export function Item({ id }) {
       </motion.div>
       <div className="card-content-container flex-vertical open">
         <motion.div
-          transition={{ duration: 0.7}}
+          transition={{ duration: 3}}
           className="card-content container" layoutId={`card-container-${id}`}>
           <motion.div
-            transition={{ duration: 0.7}}
+            transition={{ duration: 3}}
             className="card-image-container"
             layoutId={`card-image-container-${id}`}
           >
@@ -32,7 +32,7 @@ export function Item({ id }) {
 
           </motion.div>
           <motion.div
-            transition={{ duration: 0.7}}
+            transition={{ duration: 3}}
             className="title-container"
             layoutId={`title-container-${id}`}
           >
