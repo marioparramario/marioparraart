29c146d2d5936728b0091e7577303f2dc49fa86d
oqgraphic <hello@oqgraphic.com>
2020-08-14 03:05:51 +0200

    now using react-justified-grid

--- src/List.js
+++ src/List.js
@@ -3,26 +3,20 @@ import { items } from "./data";
 import "./styles/flexbin.scss";
 import { Card } from "./components/Card";
 import { photos } from "./components/photos";
-import Gallery from "react-photo-gallery";
-// import ProgressiveImage from "react-progressive-image";
+// import Gallery from "react-photo-gallery";
+import JustifiedGrid from 'react-justified-grid'
 
 export function List({ selectedId }) {
 
   return (
     <main className={`content-wrapper flex-vertical ${selectedId ? 'illustration-open' : ''}`}>
       <div className="container">
-      <Gallery photos={photos} />
-        {/* <ProgressiveImage
-        src={require("./images/yasmeen.webp")}
-        placeholder={require("./images/compressed-image.jpg")}>
-        {(src) => (
-          <img
-            src={src}
-            alt='Yasmeen Tariq'
-
-          />
-        )}
-      </ProgressiveImage> */}
+      <JustifiedGrid
+      images={photos}
+      rows={3}
+      maxRowHeight={320}
+      gutter={1}/>
+      {/* <Gallery photos={photos} /> */}
 
         <div className="card-list flexbin flexbin-margin">
           {items.map(card => (
--- src/pages/gallery.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import Gallery from "react-photo-gallery";
-import { photos } from "./components/photos";
-
-/* popout the browser and maximize to see more rows! -> */
-const BasicRows = () => <Gallery photos={photos} />;
-render(<BasicRows />, document.getElementById("app"));
-export {BasicRows};
