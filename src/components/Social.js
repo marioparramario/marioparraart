79d12174fd3ed74d808ef742e8e110c220b61d21
oqgraphic <hello@oqgraphic.com>
2020-09-09 01:01:38 +0200

    doing my best on mobile

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,8 +3,8 @@ import "../styles/social.scss";
 
 export const Social = () => (
   <div className="social flex-center">
-    <a className="flex-center" href="https://www.behance.net/oqdesign" target="_blank" rel="noopener">Behance</a>
     <a className="flex-center" href="https://www.instagram.com/marioparra.art/" target="_blank" rel="noopener">Instagram</a>
+    <a className="flex-center" href="https://www.behance.net/oqdesign" target="_blank" rel="noopener">Behance</a>
     <a className="flex-center" href="https://dribbble.com/oqdesign" target="_blank" rel="noopener">Dribbble</a>
   </div>
 );
