dabe582f1c5f2bd12548bf39b5dafd9bea08416f
oqgraphic <hello@oqgraphic.com>
2020-09-22 18:37:00 +0200

    Almost done with mobile

--- src/components/Social.js
+++ src/components/Social.js
@@ -5,6 +5,6 @@ export const Social = () => (
   <div className="social flex-center">
     <a className="flex-center" href="https://www.instagram.com/marioparra.art/" target="_blank" rel="noopener">Instagram</a>
     <a className="flex-center" href="https://www.behance.net/oqdesign" target="_blank" rel="noopener">Behance</a>
-    <a className="flex-center" href="https://dribbble.com/oqdesign" target="_blank" rel="noopener">Dribbble</a>
+    <a className="flex-center" href="https://www.artstation.com/marioparra" target="_blank" rel="noopener">Artstation</a>
   </div>
 );
