149ec16af288b6b08bd058c1ddfe4ae481989567
oqgraphic <hello@oqgraphic.com>
2020-09-23 21:20:09 +0200

    Lil tweaks

--- src/components/AboutClosing.js
+++ src/components/AboutClosing.js
@@ -16,16 +16,16 @@ export const AboutClosing = () => (
 
     <div className="column contact">
       <h3>Let’s get it touch.</h3>
-      <div className="wrapper flex">
-        <div className="item flex vertical justify-start">
+      <ul className="wrapper">
+        <li className="item flex vertical justify-start">
           <span>Email</span>
           <a href="mailto:hello@marioparra.art">hello@marioparra.art</a>
-        </div>
-        <div className="item flex vertical justify-start">
+        </li>
+        <li className="item flex vertical justify-start">
           <span>Phone</span>
           <a href="tel:+491789154238">+49 178 9154238</a>
-        </div>
-      </div>
+        </li>
+      </ul>
     </div>
   </div>
 );
