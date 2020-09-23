22aafbb6b261a938cf243dd3b77bbc892759406d
oqgraphic <hello@oqgraphic.com>
2020-09-23 22:20:18 +0200

    changing behance url

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,7 +4,7 @@ import "../styles/social.scss";
 export const Social = () => (
   <div className="social flex-center">
     <a className="flex-center" href="https://www.instagram.com/marioparra.art/" target="_blank" rel="noopener">Instagram</a>
-    <a className="flex-center" href="https://www.behance.net/oqdesign" target="_blank" rel="noopener">Behance</a>
+    <a className="flex-center" href="https://www.behance.net/mario-parra" target="_blank" rel="noopener">Behance</a>
     <a className="flex-center" href="https://www.artstation.com/marioparra" target="_blank" rel="noopener">Artstation</a>
   </div>
 );
